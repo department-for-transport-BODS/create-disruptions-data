06a2802a0ae4878d1346f8e22ba868cc4e4ee52e
Harrison Duffield <46687573+HarrisonDuffield@users.noreply.github.com>
2026-06-22 17:18:55 +0100

    OpenNext pint force

--- stacks/SiteStack.ts
+++ stacks/SiteStack.ts
@@ -61,6 +61,7 @@ export const SiteStack = ({ stack }: StackContext) => {
 
     const site = new NextjsSite(stack, "Site", {
         path: "site/",
+        openNextVersion: "4.0.3",
         runtime: "nodejs20.x",
         warm: stack.stage === "prod" || stack.stage === "preprod" ? 50 : 10,
         memorySize: stack.stage === "prod" ? "3072 MB" : "1024 MB",
